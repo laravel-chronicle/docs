b211b5fd0463043be4c4e925e07db47d4b5497df
Vasileios Ntoufoudis <info@ntoufoudis.com>
2026-07-07 14:29:12 +0300

    add matomo

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -32,13 +32,19 @@ const config: Config = {
         theme: {
           customCss: './src/css/custom.css',
         },
-        gtag: {
-            trackingID: 'G-H96MWYYX92',
-        },
       } satisfies Preset.Options,
     ],
   ],
+  plugins: [
+    'docusaurus-plugin-matomo',
+  ],
   themeConfig: {
+    matomo: {
+      matomoUrl: 'https://analytics.laravel-chronicle.dev',
+      siteId: '1',
+      phpLoader: 'matomo.php',
+      jsLoader: 'matomo.js',
+    },
     image: 'img/docusaurus-social-card.jpg',
     colorMode: {
       respectPrefersColorScheme: true,
